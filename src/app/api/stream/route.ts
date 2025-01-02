eaed6b86a439276f0db7b92cf599b592a1b0ac9b
Henry Li <henry1943@163.com>
2025-01-02 11:47:58 +0800

    chore: shorten the demo speech

--- src/app/api/stream/route.ts
+++ src/app/api/stream/route.ts
@@ -15,10 +15,10 @@ export async function GET(request: NextRequest) {
   cosyVoiceService.speak(
     "AI 就像一个超级聪明的机器人大脑，它能听懂和说很多很多的话。",
   );
-  cosyVoiceService.speak("举一个例子，AI 可以回答你任何问题，");
-  cosyVoiceService.speak("还能够为你画图，");
-  cosyVoiceService.speak("甚至为你写代码。");
-  cosyVoiceService.speak("未来还能替代人类，驾驶汽车，从事繁重的工作。");
+  // cosyVoiceService.speak("举一个例子，AI 可以回答你任何问题，");
+  // cosyVoiceService.speak("还能够为你画图，");
+  // cosyVoiceService.speak("甚至为你写代码。");
+  // cosyVoiceService.speak("未来还能替代人类，驾驶汽车，从事繁重的工作。");
   void cosyVoiceService.stopSynthesis();
 
   return new Response(cosyVoiceService.audioStream, {
